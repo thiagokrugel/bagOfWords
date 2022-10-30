5c66f9933083e0e845fa54b5df54f706545f13cb
Thiago Krügel <84471991+thiagokrugel@users.noreply.github.com>
2022-10-30 18:30:55 -0300

    i am dying

--- bagofwords.py
+++ bagofwords.py
@@ -1,21 +1,21 @@
 # Feito por: Thiago Krügel
 '''
 ENUNCIADO 
-Sua  tarefa  será  transformar  um  conjunto  de  5  sites,  sobre  o  tema  de  processamento  de 
-linguagem natural em um conjunto de cinco listas distintas de sentenças. Ou seja, você fará uma função 
-que, usando a biblioteca Beautifull Soap, faça a requisição de uma url, e extrai todas as sentenças desta 
-url. Duas condições são importantes:  
-a) A página web (url) deve apontar para uma página web em inglês contendo, não menos que 
-1000 palavras.  
-b) O texto desta página deverá ser transformado em um array de senteças.  
-
-Para separar as sentenças você pode usar os sinais de pontuação ou as funções da biblibioteca
-Spacy.
+Sua tarefa será  gerar a matriz termo documento, dos documentos recuperados da internet e 
+imprimir esta matriz na tela. Para tanto: 
+a) Considere que todas as listas de sentenças devem ser transformadas em listas de vetores, 
+onde cada item será uma das palavras da sentença. 
+b) Todos  os  vetores  devem  ser  unidos  em  um  corpus  único  formando  uma  lista  de  vetores, 
+onde cada item será um lexema.  
+c) Este único corpus será usado para gerar o vocabulário. 
+d) O  resultado  esperado  será  uma  matriz  termo  documento  criada  a  partir  da  aplicação  da 
+técnica bag of Words em todo o corpus.  
 '''
 from bs4 import BeautifulSoup
 from bs4.element import Comment
 import urllib.request
 import spacy
+import re
 
 nlp = spacy.load("en_core_web_sm")
 
@@ -34,7 +34,6 @@ def pegarTexto(body):
     visible_texts = filter(definirTexto, texts)
     return u" ".join(t.strip() for t in visible_texts)
 
-
 corpus = []
 
 html = urllib.request.urlopen('https://hbr.org/2022/04/the-power-of-natural-language-processing').read()
@@ -47,14 +46,12 @@ page = nlp(pegarTexto(html2))
 for sentence in page.sents:
     corpus.append(sentence.text)
 
-html3 = urllib.request.urlopen(
-    'https://www.datarobot.com/blog/what-is-natural-language-processing-introduction-to-nlp/').read()
+html3 = urllib.request.urlopen('https://www.datarobot.com/blog/what-is-natural-language-processing-introduction-to-nlp/').read()
 page = nlp(pegarTexto(html3))
 for sentence in page.sents:
     corpus.append(sentence.text)
 
-html4 = urllib.request.urlopen(
-    'https://www.sas.com/en_us/insights/analytics/what-is-natural-language-processing-nlp.html').read()
+html4 = urllib.request.urlopen('https://www.sas.com/en_us/insights/analytics/what-is-natural-language-processing-nlp.html').read()
 page = nlp(pegarTexto(html4))
 for sentence in page.sents:
     corpus.append(sentence.text)
@@ -70,22 +67,25 @@ def criarVocabulario(corpus):
 
     for sentenca in corpus:
         for palavra in sentenca.split():
+            #palavraSplit = re.split('[,()"-&^%$#@!*/\;~.`]', palavra)
+            #print(palavraSplit)
             vocabulario.add(palavra)
 
     return sorted(vocabulario)
 
 
 def criarMatriz(corpus):
-    vocabulario = criarVocabulario(corpus)
-    bagOfWords = []
+     vocabulario = criarVocabulario(corpus)
+     bagOfWords = []
 
-    for sentenca in corpus:
-        vetor = [0] * len(vocabulario)
-        for palavra in sentenca.split():
-            vetor[vocabulario.index(palavra)] += 1
-        bagOfWords.append(vetor)
 
-    return bagOfWords
+     for sentenca in corpus:
+         vetor = [0] * len(vocabulario)
+         for palavra in sentenca.split():
+             vetor[vocabulario.index(palavra)] += 1
+         bagOfWords.append(vetor)
+
+     return bagOfWords
 
 
 vocabulario = criarVocabulario(corpus)
